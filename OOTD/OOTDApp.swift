e64cc58c03e7c01213c23f333f3803b866c55c1a
Kuei-tsen-liao <sand964@gmail.com>
2022-06-07 23:39:59 -0700

    Initial Commit

---
+++ OOTD/OOTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  OOTDApp.swift
+//  OOTD
+//
+//  Created by sand liao on 07/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct OOTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
